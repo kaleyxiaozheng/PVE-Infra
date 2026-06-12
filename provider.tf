951b4627527f9c41f4ee53d82726a9ea07401df8
Kaley Zheng <zheng.kaley.x@gmail.com>
2026-06-12 23:41:33 +1000

    update readme

--- provider.tf
+++ provider.tf
@@ -10,6 +10,6 @@ terraform {
 provider "proxmox" {
   pm_api_url          = "https://192.168.50.100:8006/api2/json"
   pm_api_token_id     = "root@pam!terraform-token"
-  pm_api_token_secret = "d6ba2358-e8dc-4c77-b198-60637dc01075" # 使用你之前创建的那个 Secret
+  pm_api_token_secret = "d6ba2358-e8dc-4c77-b198-60637dc01075"
   pm_tls_insecure     = true
 }
