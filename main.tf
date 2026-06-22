47db9dc16910b4e016ff6ac9aae09083d93b277c
Kaley Zheng <zheng.kaley.x@gmail.com>
2026-06-22 11:00:13 +1000

    udpate node id

--- main.tf
+++ main.tf
@@ -18,11 +18,11 @@ module "worker_node" {
   count     = 3
   source    = "git@github.com:kaleyxiaozheng/k3s-node-module.git?ref=v1.0.0"
   node_name = "worker-node-${count.index + 1}"
-  vm_id     = 101 + count.index
+  vm_id     = 102 + count.index
   memory    = 3072
 
   # assign unique static IPs to each worker node
-  static_ip = "192.168.50.${101 + count.index}/24"
+  static_ip = "192.168.50.${102 + count.index}/24"
   gateway   = "192.168.50.1"
   
   user_data = templatefile(local.k3s_templates.worker, {
